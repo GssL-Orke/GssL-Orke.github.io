19c509ee85ef1978598a7ea67606aa786ccd4246
GssL-Orke <gssl@viessmann.com>
2020-03-26 13:18:20 +0100

    Update WetterParam.js

--- js/WetterParam.js
+++ js/WetterParam.js
@@ -96,7 +96,7 @@
 
             if (isValidDate(infoObj.startDate) && isValidDate(infoObj.endDate)) {
                 tableau.connectionData = JSON.stringify(infoObj); // Use this variable to pass data to your getSchema and getData functions
-                tableau.connectionName = "Wetterdaten"; // This will be the data source name in Tableau
+                tableau.connectionName = "Wetterdaten Station " + infoObj.stationnumber; // This will be the data source name in Tableau
                 tableau.submit(); // This sends the connector object to Tableau
             } else {
                 $('#errorMsg').html("Enter valid dates. For example, 2016-05-08.");
